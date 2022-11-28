409fa1273046e5491e41ef9a78ee5c29f1e8d2c0
shwg8986 <sho8986go@gmail.com>
2022-11-29 00:18:18 +0900

    revised some bags

--- Assets/Main/Scripts/NetworkedGrabbing.cs
+++ Assets/Main/Scripts/NetworkedGrabbing.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class NetworkedGrabbing : MonoBehaviour
+public class NetworkedGrabbing : MonoBehaviourPunCallbacks, IPunOwnershipCallbacks
 {
+
+    PhotonView m_photonView;
+
+    private void Awake()
+    {
+        m_photonView = GetComponent<PhotonView>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-
+       
     }
 
     // Update is called once per frame
@@ -16,13 +26,35 @@ public class NetworkedGrabbing : MonoBehaviour
 
     }
 
+    private void TransferOwnership()
+    { 
+        m_photonView.RequestOwnership();
+    }
+
     public void OnSelectEntered()
     {
         Debug.Log("Grabbed");
+        TransferOwnership();
     }
 
-    public void OnSelectedExisted()
+    public void OnSelectedExited()
     {
         Debug.Log("Released");
+    }
+
+    public void OnOwnershipRequest(PhotonView targetView, Player requestingPlayer)
+    {
+        Debug.Log($"Ownership Requested for: {targetView.name} from {requestingPlayer.NickName}");
+        m_photonView.TransferOwnership(requestingPlayer);
+    }
+
+    public void OnOwnershipTransfered(PhotonView targetView, Player previousOwner)
+    {
+        Debug.Log($"OnOwnership Transfered to {targetView.name} from {previousOwner.NickName}");
+    }
+
+    public void OnOwnershipTransferFailed(PhotonView targetView, Player senderOfFailedRequest)
+    {
+        
     }
 }
