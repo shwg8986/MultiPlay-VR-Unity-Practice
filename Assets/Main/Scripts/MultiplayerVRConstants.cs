2864be57ac9b68a3badf39863b92f090aaee92f1
shwg8986 <sho8986go@gmail.com>
2022-12-02 21:41:42 +0900

    Local Avatar Selectionの実装

--- Assets/Main/Scripts/MultiplayerVRConstants.cs
+++ Assets/Main/Scripts/MultiplayerVRConstants.cs
@@ -5,4 +5,5 @@ public class MultiplayerVRConstants
     public const string MAP_TYPE_VALUE_SCHOOL = "school";
     public const string MAP_TYPE_VALUE_OUTDOOR = "outdoor";
 
+    public static object AVATAR_SELECTION_NUMBER { get; internal set; }
 }
